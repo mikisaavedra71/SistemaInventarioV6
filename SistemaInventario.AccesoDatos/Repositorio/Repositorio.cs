44c270b5c5c41e96994a17054d1b35002e33f5c9
Miguel Saavedra <miguelsaavedrame@gmail.com>
2023-07-05 14:23:32 -0400

    Paginacion

--- SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -2,6 +2,7 @@
 using SistemaInventario.AccesoDatos.Data;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
 using SistemaInventario.Modelos;
+using SistemaInventario.Modelos.Especificaciones;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -79,6 +80,30 @@ namespace SistemaInventario.AccesoDatos.Repositorio
             }
             return  await query.ToListAsync();
         }
+        public PagedList<T> ObtenerTodosPaginado(Parametros parametros, Expression<Func<T, bool>> filtro, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, string incluirPropiedades, bool isTracking)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro); // select /* from where ....
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluitProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluitProp); //ejemplo Categoria,Marca
+                }
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return PagedList<T>.ToPagedList(query, parametros.PageNumber, parametros.PageSize);
+        }
 
         void IRepositorio<T>.Remover(T entidad)
         {
@@ -89,5 +114,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio
         {
             dbSet.RemoveRange(entidad);
         }
+
+       
     }
 }
